229d862a6023957311769816ed5f454ec43a4fb8
amamFz <122434192+amamFz@users.noreply.github.com>
2023-01-17 19:39:20 +0700

    header.h
    Identitas(char jud[50]=""){
     if(strcmp(jud,"Identitas")==0)
     {
    	cout<<"=============================================================="<<endl;
       cout<<"=========================IDENTITAS============================"<<endl;
       cout<<"NAMA		: ILHAM FAUZI "<<endl;
       cout<<"KELAS		: X-PPLG      "<<endl;
       cout<<"NO.HP		: 082127009275"<<endl;
       cout<<"USERNAME	: ADMIN       "<<endl;
       cout<<"PASSWORD	: 1234567     "<<endl;
       cout<<"=============================================================="<<endl;
       }
    }

---
+++ Identitas.h
@@ -0,0 +1,13 @@
+Identitas(char jud[50]=""){
+ if(strcmp(jud,"Identitas")==0)
+ {
+	cout<<"=============================================================="<<endl;
+   cout<<"=========================IDENTITAS============================"<<endl;
+   cout<<"NAMA		: ILHAM FAUZI "<<endl;
+   cout<<"KELAS		: X-PPLG      "<<endl;
+   cout<<"NO.HP		: 082127009275"<<endl;
+   cout<<"USERNAME	: ADMIN       "<<endl;
+   cout<<"PASSWORD	: 1234567     "<<endl;
+   cout<<"=============================================================="<<endl;
+   }
+}
